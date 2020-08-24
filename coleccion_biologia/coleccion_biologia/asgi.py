8237e3a7ff2c36d98f858f5eae9bb791b3d1f1fc
Pablo Sao <pablo_sao@outlook.com>
2020-08-24 17:51:48 -0600

    update

--- coleccion_biologia/coleccion_biologia/asgi.py
+++ coleccion_biologia/coleccion_biologia/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for colecciones_biologia_api project.
+ASGI config for coleccion_biologia project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'colecciones_biologia_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coleccion_biologia.settings')
 
 application = get_asgi_application()
--- coleccion_biologia/manage.py
+++ coleccion_biologia/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'colecciones_biologia_api.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coleccion_biologia.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
